e53026f4742efffd3568724489b9a0d6697616b8
hokageCV <chaitanyavaru@gmail.com>
2025-01-17 23:17:23 +0530

    Add keyboard shortcut for sidebar toggle

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,12 +1,27 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="sidebar"
 export default class extends Controller {
-  toggle(e) {
-    e.preventDefault();
+  connect() {
+    document.addEventListener('keydown', this.handleShortcut.bind(this))
+  }
 
-    let newState = this.element.dataset.expanded === 'true' ? 'false' : 'true';
-    this.element.dataset.expanded = newState;
-    document.cookie = `sidebar_expanded=${newState}`;
+  disconnect() {
+    document.removeEventListener('keydown', this.handleShortcut.bind(this))
+  }
+
+  handleShortcut(e) {
+    if (e.ctrlKey && e.key === 'd') {
+      e.preventDefault()
+      this.toggle(e)
+    }
+  }
+
+  toggle(e) {
+    e.preventDefault()
+
+    let newState = this.element.dataset.expanded === 'true' ? 'false' : 'true'
+    this.element.dataset.expanded = newState
+    document.cookie = `sidebar_expanded=${newState}`
   }
 }
