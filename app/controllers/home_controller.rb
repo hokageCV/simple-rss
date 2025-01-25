5cd47d53a1075c83ef525174464accf66ba641fa
hokageCV <chaitanyavaru@gmail.com>
2025-01-25 17:51:20 +0530

    Show only recent articles in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @user = Current.user
-    @articles = @user.articles.recent_first
+    @articles = @user.articles.unread.recent_first.of_this_week
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,15 @@ class Article < ApplicationRecord
   belongs_to :user
 
   scope :recent_first, -> { order(published_at: :desc) }
+  scope :of_this_week, -> {
+    where("published_at >= ?", Time.current.beginning_of_week)
+    .where("published_at <= ?", Time.current)
+  }
+  scope :of_this_month, -> {
+    where("published_at >= ?", Time.current.beginning_of_month)
+    .where("published_at <= ?", Time.current)
+  }
+  scope :unread, -> { where(status: "unread") }
 
   STATUSES = { unread: "unread", read: "read" }.freeze
   validates :status, inclusion: { in: STATUSES.values }
