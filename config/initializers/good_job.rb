8455690387cd2e7a53366a817de25fdd7991c76f
hokageCV <chaitanyavaru@gmail.com>
2026-05-17 20:42:27 +0530

    monkeypath to  remove good_job polling logs

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,2 +1,14 @@
 Rails.application.config.good_job ||= {}
 Rails.application.config.good_job[:enable_listen_notifications] = false
+
+# Keep polling but suppress noisy SQL logs from GoodJob's poller
+module GoodJobSqlSilencer
+  def sql(event)
+    return if event.payload[:name]&.start_with?("GoodJob::")
+    super
+  end
+end
+
+Rails.application.config.after_initialize do
+  ActiveRecord::LogSubscriber.prepend(GoodJobSqlSilencer)
+end
